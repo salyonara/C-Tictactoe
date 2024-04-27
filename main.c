39816579af54acaca0f7fe22074b735910a69e4a
salyonara <168229674+salyonara@users.noreply.github.com>
2024-04-27 19:32:26 +0200

    Update main.c

--- main.c
+++ main.c
@@ -9,9 +9,9 @@ int main()
     int turn = X;
     while(1)
     {
-        REFRESH;
-        draw(board);
-        Winner(&key);
+        REFRESH; //system("cls");
+        draw(board); //draw tic tac toe board
+        Winner(&key); //Check if one of the players won
         if(key == 99)
         {
             break;
@@ -19,8 +19,8 @@ int main()
 
         if(turn == X)
         {
-            printf("X\n");
-            key = select();
+            printf("X\n"); //tell which turn it is
+            key = select(); 
             if(board[key] == EMPTY)
             {
                 board[key] = X;
@@ -42,7 +42,7 @@ int main()
             }
             else
             {
-                continue;
+                continue; //I'm having a doubt
             }
         }
     }
