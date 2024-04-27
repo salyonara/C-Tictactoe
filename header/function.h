018751403f0b6ee71d5b19ce7ef3543027541fb9
salyonara <168229674+salyonara@users.noreply.github.com>
2024-04-27 18:19:36 +0200

    Update function.h

--- header/function.h
+++ header/function.h
@@ -9,11 +9,12 @@
 void draw(int*);    //DONE
 int select();   
 void Winner(int*);
-void Win(int*);
+
+/*These are the winning hands Vx,Hx and Dx. Vx > VerticalX moves, Hx > HorizontalX moves and Dx > DiagonalX moves*/
 #define Vx board[0] == X && board[10] == X && board[20] == X || board[2] == X && board[12] == X && board[22] == X || board[4] == X && board[14] == X && board[24] == X
 #define Hx board[0] == X && board[2] == X && board[4] == X || board[10] == X && board[12] == X && board[14] == X || board[20] == X && board[22] == X && board[24] == X
 #define Dx board[0] == X && board[12] == X && board[24] == X || board[4] == X && board[12] == X && board[20] == X
-
+/*These are the winning hands Vo,Ho and Do. Vo > VerticalO moves, Ho > HorizontalO moves and Do > DiagonalO moves*/
 #define Vo board[0] == O && board[10] == O && board[20] == O || board[2] == O && board[12] == O && board[22] == O || board[4] == O && board[14] == O && board[24] == O
 #define Ho board[0] == O && board[2] == O && board[4] == O || board[10] == O && board[12] == O && board[14] == O || board[20] == O && board[22] == O && board[24] == O
 #define Do board[0] == O && board[12] == O && board[24] == O || board[4] == O && board[12] == O && board[20] == O
